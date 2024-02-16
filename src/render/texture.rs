8eb7f2eae92a7fc65ef08eb460682e5a1d308e42
truenotzero <truenotzero@gmail.com>
2024-02-17 01:06:46 +0200

    Started working on texture atlasses

--- src/render/texture.rs
+++ src/render/texture.rs
@@ -1,3 +1,5 @@
+use std::ops::Index;
+
 use crate::math::IVec2;
 
 #[derive(Clone, Copy)]
@@ -6,40 +8,86 @@ pub enum Filtering {
     Nearest,
 }
 
-pub trait ITexture {}
+pub trait ITexture {
+    fn width(&self) -> i32;
+    fn height(&self) -> i32;
+}
 
-pub trait IAtlas {}
+pub trait IAtlasColumn: Index<usize> {
+    type Texture: ITexture;
+}
+
+pub trait IAtlas: Index<usize> {
+    type Texture: ITexture;
+}
 
 pub trait IAtlasBuilder: Default {
     type Out: IAtlas;
 
-    fn texture_dimensions(dim: IVec2) -> Result<Self, ()>;
+    fn texture_dimensions(self, dim: IVec2) -> Result<Self, ()>;
     fn build(self) -> Result<Self::Out, ()>;
 }
 
 #[cfg(feature = "gl45")]
 mod imp {
+    use std::ops::Index;
+
     use crate::math::IVec2;
     use crate::render::api as gl;
 
     use super::IAtlas;
     use super::IAtlasBuilder;
+    use super::ITexture;
+
+    pub struct Texture;
+
+    impl ITexture for Texture {
+        fn width(&self) -> i32 {
+            todo!()
+        }
+
+        fn height(&self) -> i32 {
+            todo!()
+        }
+    }
+
+    pub struct AtlasRow;
 
     pub struct Atlas;
 
-    impl IAtlas for Atlas {}
+    impl Index<usize> for Atlas {
+        type Output=AtlasRow;
+
+        fn index(&self, index: usize) -> &Self::Output {
+            todo!()
+        }
+    }
+
+    impl IAtlas for Atlas {
+        type Texture=Texture;
+    }
 
     #[derive(Default)]
-    pub struct AtlasBuilder(gl::Texture);
+    pub struct AtlasBuilder {
+        o: gl::Texture,
+        dim: Option<IVec2>,
+    }
 
     impl IAtlasBuilder for AtlasBuilder {
         type Out = Atlas;
 
-        fn texture_dimensions(dim: IVec2) -> Result<Self, ()> {
-            todo!()
+        fn texture_dimensions(self, dim: IVec2) -> Result<Self, ()> {
+            Ok(Self {
+                dim: Some(dim),
+                ..self
+            })
         }
 
         fn build(self) -> Result<Self::Out, ()> {
+            gl::verify! {
+                gl::TexImage2D(gl::TEXTURE_RECTANGLE, 0, gl::RGBA as _, todo!(), todo!(), 0, gl::RGBA, gl::UNSIGNED_BYTE, todo!());
+            }
+
             todo!()
         }
     }
