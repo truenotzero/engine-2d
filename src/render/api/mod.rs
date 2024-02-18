5eff0a09637cce5a883372e714726d580e81b3d5
truenotzero <truenotzero@gmail.com>
2024-02-18 22:37:12 +0200

    Exported GLFW, windowing API will be done later

--- src/render/api/mod.rs
+++ src/render/api/mod.rs
@@ -3,3 +3,10 @@ mod gl45;
 
 #[cfg(feature = "gl45")]
 pub use gl45::*;
+
+// TODO: windowing API
+// for now, due to time constraints
+// re-export glfw
+
+#[cfg(feature = "glfw")]
+pub extern crate glfw;
